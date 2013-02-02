67bfa79a505133a111f53696641a674b113882b6
Simon Busch <morphis@gravedo.de>
2013-02-02 18:30:42 +0100

    Small changes and minor improvements
    Open-WebOS-DCO-1.0-Signed-Off-By: Simon Busch <morphis@gravedo.de>

--- source/Backend.js
+++ source/Backend.js
@@ -1,9 +1,15 @@
 enyo.kind({
 	name: "Backend",
-	kind: "enyo.Component",
-	constructor: function() {
+	kind: enyo.Component,
+	pinCard: null,
+	components: [
+		{ kind: "Signals", ondeviceready: "deviceready" },
+	],
+	create: function() {
+		this.inherited(arguments);
+	},
+	deviceready: function(inSender, inEvent) {
+		this.inherited(arguments);
 		enyo.log("Constructing Backend");
-		
-		//Subscribe to services here, talk to PhoneApp via enyo.Signals.send()
 	},
 });
